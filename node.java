94325fb2f176cdfee99416b0e3541ca93b292efc
gargshikhar <shikhargarg1992@gmail.com>
2015-02-02 16:20:39 -0800

    Update node.java

--- node.java
+++ node.java
@@ -1,9 +1,17 @@
+
+// =============== BEGIN ASSESSMENT HEADER ================
+/// @file node.java
+/// @author Shikhar Garg [shikhargarg1992@gmail.com]
+/// @date January 13, 2014
+/// @program description  Constructor for creating a node
+// ================== END ASSESSMENT HEADER ===============
+
 public class node {
   int data;
   node next;
-  
-  public node(int d, node n){
-	  data = d;
-	  next = n;
+   
+  public node(int d, node n){                               // Constructor for creating a node 
+	 data = d;
+	 next = n;
   }
 }
